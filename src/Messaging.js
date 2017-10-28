2a1218bce3d380aebd5310a428e92d9a5777c1a8
Julian Kalogerakis <jmk833@nyu.edu>
2017-10-28 08:58:06 -0400

    templates and pages almost 100% donegit add *git add *

--- src/Messaging.js
+++ src/Messaging.js
@@ -15,7 +15,7 @@ function sendMessage(to, from, body){
     });
 }
 
-sendMessage("+13477612839", "+16467913643", "mongo");
+//sendMessage("+13477612839", "+16467913643", "mongo");
 
 module.exports = {
     sendMessage:sendMessage
--- src/app.js
+++ src/app.js
@@ -2,10 +2,12 @@ const fs = require('fs');
 const express = require("express");
 //const db = require('./c4g.js');
 const messenger = require('./Messaging.js');
+const bodyParser = require('body-parser');
 
 const app = express();
 app.set('view engine', 'hbs');
 app.use(express.static(__dirname));
+app.use(bodyParser.urlencoded({extended: false}));
 
 const data = {
 	'families': {
@@ -17,12 +19,16 @@ const data = {
 			"housing-status": "homeless",
 			"employment-status": "unemployed",
 			"average-response-time": "3 weeks",
-			"message-history-length": 3,
 			"message-history": [
-								["hey David! how is the job search going?", "outgoing", "8/1/17"],
-								["It's going OK. Still haven't found anything that will help to afford a lease.", "incoming", "8/22/17"],
-								["Don't you worry, I'm here to help. Check out this jop opening at jobs.com", "outgoing", "8/23/17"]
-								]
+								["hey David! how is the job search going?", true, "8/1/17"],
+								["It's going OK. Still haven't found anything that will help to afford a lease.", false, "8/22/17"],
+								["Don't you worry, I'm here to help. Check out this jop opening at jobs.com", true, "8/23/17"]
+								],
+			"message-history-reverse":[
+								["Don't you worry, I'm here to help. Check out this jop opening at jobs.com", true, "8/23/17"],
+								["It's going OK. Still haven't found anything that will help to afford a lease.", false, "8/22/17"],
+								["hey David! how is the job search going?", true, "8/1/17"]
+									]
 		},
 		"Miller": {
 			"phone": "+13477612839",
@@ -32,14 +38,20 @@ const data = {
 			"housing-status": "housed",
 			"employment-status": "unemployed",
 			"average-response-time": "1 week",
-			"message-history-length": 5,
 			"message-history": [
-								["hey Percy! how is the job search going? Let's do our best to make sure you do not get evicted", "outgoing", "10/5/17"],
-								["Thanks for reaching out. I actually haven't been looking for a job. The laws protect us for a short while.", "incoming", "10/12/17"],
-								["Sounds like you need to change your attitude! Think about your family's future! Find that job soon.", "outgoing", "10/13/17"],
-								["After reconsidering a few things, I decided to go to a few job fairs this week. Will let you know how it goes.", "incoming", "10/20/17"],
-								["That's what I like to hear. Text me as soon as you have more information.", "outgoing", "10/13/17"],
-								]
+								["hey Percy! how is the job search going? Let's do our best to make sure you do not get evicted", true, "10/5/17"],
+								["Thanks for reaching out. I actually haven't been looking for a job. The laws protect us for a short while.", false, "10/12/17"],
+								["Sounds like you need to change your attitude! Think about your family's future! Find that job soon.", true, "10/13/17"],
+								["After reconsidering a few things, I decided to go to a few job fairs this week. Will let you know how it goes.", false, "10/20/17"],
+								["That's what I like to hear. Text me as soon as you have more information.", true, "10/13/17"]
+								],
+			"message-history-reverse":[
+								["That's what I like to hear. Text me as soon as you have more information.", true, "10/13/17"],
+								["After reconsidering a few things, I decided to go to a few job fairs this week. Will let you know how it goes.", false, "10/20/17"],
+								["Sounds like you need to change your attitude! Think about your family's future! Find that job soon.", true, "10/13/17"],
+								["Thanks for reaching out. I actually haven't been looking for a job. The laws protect us for a short while.", false, "10/12/17"],
+								["hey Percy! how is the job search going? Let's do our best to make sure you do not get evicted", true, "10/5/17"]
+										]
 		},
 		"Brown": {
 			"phone": "+13477612839",
@@ -49,11 +61,14 @@ const data = {
 			"housing-status": "housed",
 			"employment-status": "employed",
 			"average-response-time": "1 week",
-			"message-history-length": 2,
 			"message-history": [
-								["Hey Jackie! Just heard you got a job and a house. Looks like you've done my work for me", "outgoing", "10/5/17"],
-								["Thanks. I will be sure to let you know if my situation changes.", "incoming", "10/12/17"],
-								]
+								["Hey Jackie! Just heard you got a job and a house. Looks like you've done my work for me", true, "10/5/17"],
+								["Thanks. I will be sure to let you know if my situation changes.", false, "10/12/17"]
+								],
+			"message-history-reverse":[
+								["Thanks. I will be sure to let you know if my situation changes.", false, "10/12/17"],
+								["Hey Jackie! Just heard you got a job and a house. Looks like you've done my work for me", true, "10/5/17"]
+										]	
 		},
 		"Chase": {
 			"phone": "+13477612839",
@@ -63,11 +78,14 @@ const data = {
 			"housing-status": "housed",
 			"employment-status": "employed",
 			"average-response-time": "1 day",
-			"message-history-length": 2,
 			"message-history": [
-								["Hey Morgan, not sure why you are on here. Obviously you are in good standing.", "outgoing", "10/24/17"],
-								["Yeah I know.", "incoming", "10/25/17"],
-								]
+								["Hey Morgan, not sure why you are on here. Obviously you are in good standing.", true, "10/24/17"],
+								["Yeah I know.", false, "10/25/17"]
+								],
+			"message-history-reverse": [
+								["Yeah I know.", false, "10/25/17"],
+								["Hey Morgan, not sure why you are on here. Obviously you are in good standing.", true, "10/24/17"]
+										]
 		},
 		"Mark": {
 			"phone": "+13477612839",
@@ -77,15 +95,23 @@ const data = {
 			"housing-status": "homeless",
 			"employment-status": "unemployed",
 			"average-response-time": "1 week",
-			"message-history-length": 6,
 			"message-history": [
-								["Hey", "outgoing", "9/4/17"],
-								["Hey", "incoming", "9/11/17"],
-								["Hey", "outgoing", "9/18/17"],
-								["Hey", "incoming", "9/25/17"],
-								["Hey", "outgoing", "10/2/17"],
-								["Hey", "incoming", "10/9/17"],
-								]
+								["Hey", true, "9/4/17"],
+								["Hey", false, "9/11/17"],
+								["Hey", false, "9/18/17"],
+								["Hey", true, "9/25/17"],
+								["Hey", false, "10/2/17"],
+								["Hey", true, "10/9/17"]
+								],
+			"message-history-reverse": [
+								["Hey", true, "10/9/17"],
+								["Hey", false, "10/2/17"],
+								["Hey", true, "9/25/17"],
+								["Hey", false, "9/18/17"],
+								["Hey", true, "9/11/17"],
+								["Hey", false, "9/4/17"]
+										]
+
 		},
 		"Gordon": {
 			"phone": "+13477612839",
@@ -95,15 +121,22 @@ const data = {
 			"housing-status": "homeless",
 			"employment-status": "unemployed",
 			"average-response-time": "1 week",
-			"message-history-length": 6,
 			"message-history": [
-								["Hey", "outgoing", "9/4/17"],
-								["Hey", "incoming", "9/11/17"],
-								["Hey", "outgoing", "9/18/17"],
-								["Hey", "incoming", "9/25/17"],
-								["Hey", "outgoing", "10/2/17"],
-								["Hey", "incoming", "10/9/17"],
-								]
+								["Hey", false, "9/4/17"],
+								["Hey", true, "9/11/17"],
+								["Hey", false, "9/18/17"],
+								["Hey", true, "9/25/17"],
+								["Hey", false, "10/2/17"],
+								["Hey", true, "10/9/17"]
+								],
+			"message-history-reverse": [
+								["Hey", true, "10/9/17"],
+								["Hey", false, "10/2/17"],
+								["Hey", true, "9/25/17"],
+								["Hey", false, "9/18/17"],
+								["Hey", true, "9/11/17"],
+								["Hey", false, "9/4/17"]
+										]
 		},
 		"Rose": {
 			"phone": "+13477612839",
@@ -113,15 +146,22 @@ const data = {
 			"housing-status": "homeless",
 			"employment-status": "unemployed",
 			"average-response-time": "1 week",
-			"message-history-length": 6,
 			"message-history": [
-								["Hey", "outgoing", "9/4/17"],
-								["Hey", "incoming", "9/11/17"],
-								["Hey", "outgoing", "9/18/17"],
-								["Hey", "incoming", "9/25/17"],
-								["Hey", "outgoing", "10/2/17"],
-								["Hey", "incoming", "10/9/17"],
-								]
+								["Hey", false, "9/4/17"],
+								["Hey", true, "9/11/17"],
+								["Hey", false, "9/18/17"],
+								["Hey", true, "9/25/17"],
+								["Hey", false, "10/2/17"],
+								["Hey", true, "10/9/17"]
+								],
+			"message-history-reverse": [
+								["Hey", true, "10/9/17"],
+								["Hey", false, "10/2/17"],
+								["Hey", true, "9/25/17"],
+								["Hey", false, "9/18/17"],
+								["Hey", true, "9/11/17"],
+								["Hey", false, "9/4/17"]
+										]
 		},
 	},
 	'alerts': [
@@ -141,7 +181,7 @@ app.get('/', function(req, res){
 });
 
 app.get('/alerts', function(req, res){
-	res.render("alerts");
+	res.render("alerts", {body:data['alerts']});
 });
 
 app.get('/setReminder', function(req, res){
@@ -160,7 +200,29 @@ app.get('/familyProfile', function(req, res){
 });
 
 app.get("/editinfo", function(req, res){
-	res.render("editinfo");
+	const fam = req.query.family;
+	console.log(fam);
+	console.log(data["families"][fam]);
+	res.render("editinfo", {body:data["families"][fam], name:fam});
+});
+
+app.post("/editinfo", function(req, res){
+	const fam = req.query.family;
+	const body = req.body;
+	if(data['families'][fam]['family-members'].includes(body.representative)){
+		console.log("inside");
+		data['families'][fam]['representative'] = body.representative;
+	}
+	if(body['housing-status'] === "housed" || body['housing-status'] === "homeless"){
+		console.log("inside");
+		data['families'][fam]['housing-status'] = body['housing-status'];
+	}
+	if(body['employment-status'] === "employed" || body['employment-status'] === "unemployed"){
+		console.log("inside");
+		data['families'][fam]['employment-status'] = body['employment-status'];
+	}
+	console.log(req.body);
+	res.render("home");
 });
 
 app.get('/messaging', function(req, res){
@@ -168,11 +230,22 @@ app.get('/messaging', function(req, res){
 });
 
 app.get('/messageThread', function(req, res){
-	res.render("messageThread");
+	const fam = req.query.family;
+	console.log(fam);
+	console.log(data["families"][fam]);
+	res.render("chatBox", {body:data["families"][fam], name:fam, messages:data["families"][fam]["message-history"]});
 });
 
-
-//messenger.sendMessage("+13477612839", "+12015590989", "from app.js");
+app.post('/messageThread', function(req, res){
+	const fam = req.query.family;
+	console.log(fam);
+	console.log(data["families"][fam]);
+	const message = req.body.messageText;
+	messenger.sendMessage("+13477612839", "+12015590989", message);
+	data["families"][fam]["message-history"].push([""+message, true, "10/28/17"]);
+	data["families"][fam]["message-history-reverse"].unshift([""+message, true, "10/28/17"]);
+	res.render("chatBox", {body:data["families"][fam], name:fam, messages:data["families"][fam]["message-history"]});
+});
 
 app.listen(3000);
 console.log("Started server on port 3000");
