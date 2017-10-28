ab9911b4cc8fbaecac3ed92e8efad8aab57797b8
Julian Kalogerakis <jmk833@nyu.edu>
2017-10-27 22:51:21 -0400

    changes made to app.js

---
+++ src/app.js
@@ -0,0 +1,46 @@
+const fs = require('fs');
+const express = require("express");
+const db = require('./colorlib.js');
+
+const app = express();
+app.set('view engine', 'hbs');
+
+
+app.get('/signin', function(req, res){
+	res.render("signin");
+});
+
+app.get('/', function(req, res){
+	res.render("home");
+});
+
+app.get('/alerts', function(req, res){
+	res.render("alerts");
+});
+
+app.get('/setAlerts', function(req, res){
+	res.render("setAlerts");
+});
+
+app.get('/families', function(req, res){
+	res.render("families");
+});
+
+app.get('/familyProfile', function(req, res){
+	res.render("familyProfile");
+});
+
+app.get("/editinfo", function(req, res){
+	res.render("editinfo");
+});
+
+app.get('/messaging', function(req, res){
+	res.render("messaging");
+});
+
+app.get('/messageThread', function(req, res){
+	res.render("messageThread");
+});
+
+app.listen(3000);
+console.log("Started server on port 3000");
